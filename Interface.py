7cb65071500aa4e2bda406e7b5f708724f5c858f
Prachi Srivastava <87762435+PrachiSrivastava2709@users.noreply.github.com>
2023-01-27 05:24:24 +0530

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -1,7 +1,7 @@
 import APIcall as call
 
 url = "https://api.apilayer.com/currency_data/"
-header = {"apikey": "2m3OOqOry0I8PDCRG5OiKpB8li6X2RJx"}
+header = {"apikey": ----cannot share-----}
 action = call.menu()
 if action == 1:
     print(call.convert(url, header))
