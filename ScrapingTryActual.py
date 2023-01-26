5ec67e70d54b7e1ad010f8d6e1ff0906f38375cb
Prachi Srivastava <87762435+PrachiSrivastava2709@users.noreply.github.com>
2023-01-27 05:03:54 +0530

    Delete ScrapingTryActual.py

--- ScrapingTryActual.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-#html = requests.get('https://www.xe.com/currencyconverter/convert/?Amount=1&From=USD&To=INR').text
-html = requests.get('https://www.xe.com/currencyconverter/convert/?Amount=1&From=USD&To=CAD').text
-
-soup = BeautifulSoup(html, 'lxml')
-content = soup.find('p', class_ = 'result__BigRate-sc-1bsijpp-1 iGrAod')
-print(content.text)
